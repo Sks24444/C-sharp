5976402c5b823627b0ac69bbf30c91f294cb470f
Sks24444 <89986494+Sks24444@users.noreply.github.com>
2023-07-20 18:04:31 +0530

    Add files via upload

---
+++ second/Exersize 5/Exersize 5/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+namespace Excercise
+{
+    enum DuckType
+    {
+        Rubber,
+        Mallard,
+        Redhead
+    }
+
+    interface IDuck
+    {
+        double Weight { get; set; }
+        int NumberOfWings { get; set; }
+        DuckType Type { get; set; }
+
+        void Fly();
+        void Quack();
+        void ShowDetails();
+    }
+
+    class RubberDuck : IDuck
+    {
+        public double Weight { get; set; }
+        public int NumberOfWings { get; set; }
+        public DuckType Type { get; set; } = DuckType.Rubber;
+
+        public void Fly()
+        {
+            Console.WriteLine("I'm a rubber duck. I can't fly.");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Squeak! Squeak!");
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Type: {Type}");
+            Console.WriteLine($"Weight: {Weight} kg");
+            Console.WriteLine($"Number of wings: {NumberOfWings}");
+        }
+    }
+
+    class MallardDuck : IDuck
+    {
+        public double Weight { get; set; }
+        public int NumberOfWings { get; set; }
+        public DuckType Type { get; set; } = DuckType.Mallard;
+
+        public void Fly()
+        {
+            Console.WriteLine("I'm a mallard duck. I fly fast!");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Quack! Quack! Quack!");
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Type: {Type}");
+            Console.WriteLine($"Weight: {Weight} kg");
+            Console.WriteLine($"Number of wings: {NumberOfWings}");
+        }
+    }
+
+    class RedheadDuck : IDuck
+    {
+        public double Weight { get; set; }
+        public int NumberOfWings { get; set; }
+        public DuckType Type { get; set; } = DuckType.Redhead;
+
+        public void Fly()
+        {
+            Console.WriteLine("I'm a redhead duck. I fly slow.");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Quack! Quack!");
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Type: {Type}");
+            Console.WriteLine($"Weight: {Weight} kg");
+            Console.WriteLine($"Number of wings: {NumberOfWings}");
+        }
+    }
+
+    class Excercise_5
+    {
+        public static void Main()
+        {
+            // Create a rubber duck
+            IDuck rubberDuck = new RubberDuck
+            {
+                Weight = 0.4,
+                NumberOfWings = 0 // A rubber duck has no wings
+            };
+
+            // Create a mallard duck
+            IDuck mallardDuck = new MallardDuck
+            {
+                Weight = 1.9,
+                NumberOfWings = 2
+            };
+
+            // Create a redhead duck
+            IDuck redheadDuck = new RedheadDuck
+            {
+                Weight = 2.0,
+                NumberOfWings = 2
+            };
+
+            // Show the details of the ducks
+            rubberDuck.ShowDetails();
+            rubberDuck.Fly();
+            rubberDuck.Quack();
+
+            mallardDuck.ShowDetails();
+            mallardDuck.Fly();
+            mallardDuck.Quack();
+
+            redheadDuck.ShowDetails();
+            redheadDuck.Fly();
+            redheadDuck.Quack();
+        }
+    }
+}
---
+++ second/exersize 4/Program.cs
@@ -0,0 +1,143 @@
+﻿ using System;
+
+namespace EquipmentInventory
+{
+    public enum EquipmentType
+    {
+        Mobile,
+        Immobile
+    }
+
+    public interface IEquipment
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        int DistanceMoved { get; set; }
+        int MaintenanceCost { get; set; }
+        EquipmentType EquipmentType { get; set; }
+
+        void MoveBy(int distance);
+        void DisplayDetails();
+    }
+
+    public abstract class Equipment : IEquipment
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DistanceMoved { get; set; }
+        public int MaintenanceCost { get; set; }
+        public EquipmentType EquipmentType { get; set; }
+
+        public Equipment(string name, string description, EquipmentType equipmentType)
+        {
+            Name = name;
+            Description = description;
+            EquipmentType = equipmentType;
+        }
+
+        public abstract void MoveBy(int distance);
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Description: {Description}, Distance Moved: {DistanceMoved} km, Maintenance Cost: {MaintenanceCost}");
+        }
+    }
+
+    public class MobileEquipment : Equipment
+    {
+        public int NumberOfWheels { get; set; }
+
+        public MobileEquipment(string name, string description, int numberOfWheels) : base(name, description, EquipmentType.Mobile)
+        {
+            NumberOfWheels = numberOfWheels;
+        }
+
+        public override void MoveBy(int distance)
+        {
+            DistanceMoved += distance;
+            MaintenanceCost += (NumberOfWheels * distance);
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Number of Wheels: {NumberOfWheels}");
+        }
+    }
+
+    public class ImmobileEquipment : Equipment
+    {
+        public int Weight { get; set; }
+
+        public ImmobileEquipment(string name, string description, int weight) : base(name, description, EquipmentType.Immobile)
+        {
+            Weight = weight;
+        }
+
+        public override void MoveBy(int distance)
+        {
+            DistanceMoved += distance;
+            MaintenanceCost += (Weight * distance);
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Weight: {Weight}");
+        }
+    }
+
+    public class EquipmentManager
+    {
+        private IEquipment[] equipments;
+
+        public EquipmentManager(int size)
+        {
+            equipments = new IEquipment[size];
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            for (int i = 0; i < equipments.Length; i++)
+            {
+                if (equipments[i] == null)
+                {
+                    equipments[i] = equipment;
+                    break;
+                }
+            }
+        }
+
+        public void MoveEquipment(int index, int distance)
+        {
+            equipments[index]?.MoveBy(distance);
+        }
+
+        public void DisplayEquipmentDetails(int index)
+        {
+            equipments[index]?.DisplayDetails();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EquipmentManager manager = new EquipmentManager(5);
+
+            MobileEquipment jcb = new MobileEquipment("JCB", "A construction vehicle", 4);
+            ImmobileEquipment ladder = new ImmobileEquipment("Ladder", "A climbing aid", 10);
+
+            manager.AddEquipment(jcb);
+            manager.AddEquipment(ladder);
+
+            manager.MoveEquipment(0, 10);
+            manager.MoveEquipment(1, 5);
+
+            manager.DisplayEquipmentDetails(0);
+            manager.DisplayEquipmentDetails(1);
+
+            Console.ReadKey();
+        }
+    }
+}
